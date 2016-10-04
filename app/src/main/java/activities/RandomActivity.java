5b5eb46977378a50113a004b1155ac928b3b545b
Priya Sharma <psharma1@SEASIAINFOTECH.COM>
2016-10-04 17:00:24 +0530

    Added log statement

--- app/src/main/java/activities/RandomActivity.java
+++ app/src/main/java/activities/RandomActivity.java
@@ -2,6 +2,7 @@ package activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by psharma1 on 2016-10-04.
@@ -11,6 +12,7 @@ public class RandomActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("RandomActivity","Modified");
     }
     
 }
