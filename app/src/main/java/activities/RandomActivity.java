2ccc6ef6fa4b7de3ce8b92a2d1e1bfacd1fb4b49
Manpreet Kaur <mkaur3@SEASIAINFOTECH.COM>
2016-10-04 16:57:42 +0530

    Added on create method

--- app/src/main/java/activities/RandomActivity.java
+++ app/src/main/java/activities/RandomActivity.java
@@ -1,9 +1,16 @@
 package activities;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by psharma1 on 2016-10-04.
  */
 public class RandomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
 }
